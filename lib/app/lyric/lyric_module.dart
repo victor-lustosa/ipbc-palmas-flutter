40821fa98e191fa6821b1a22ec4e2d5aa8cef685
Victor Lustosa <victor.olustosa@outlook.com>
2022-11-09 19:24:52 -0300

    merge into main

---
+++ lib/app/lyric/lyric_module.dart
@@ -0,0 +1,18 @@
+import '../core/external/firestore_datasource.dart';
+import '../core/infra/repositories/repository.dart';
+import 'blocs/lyric_bloc.dart';
+import 'domain/entities/lyric_entity.dart';
+import 'domain/use-cases/lyrics_use_cases.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final lyricModule = [
+  Provider<Repository<LyricEntity>>(
+      create: (context) => Repository(
+          getDatasource:
+              FirestoreDatasource(firestore: FirebaseFirestore.instance))),
+  Provider<LyricBloc>(
+      create: (context) => LyricBloc(
+          lyricsUseCase: LyricsUseCases(
+              repository: context.read<Repository<LyricEntity>>()))),
+];
