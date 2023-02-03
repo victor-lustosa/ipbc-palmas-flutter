69d6163ad2c433ad7cf56490b785609862e5ccc4
Victor Lustosa <victor.olustosa@outlook.com>
2023-02-03 09:54:47 -0300

    corrigindo teste usecase

--- test/app/lyric/domain/use-cases/lyrics_use_cases_test.dart
+++ test/app/lyric/domain/use-cases/lyrics_use_cases_test.dart
@@ -3,18 +3,21 @@ import 'package:ipbc_palmas/app/core/infra/repositories/repository.dart';
 import 'package:ipbc_palmas/app/lyric/domain/entities/lyric_entity.dart';
 import 'package:ipbc_palmas/app/lyric/domain/use-cases/lyrics_use_cases.dart';
 import 'package:mocktail/mocktail.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../../../mocks/mocks.dart';
 
 void main() {
   test('getting lyrics in use case', () {
     Repository<LyricEntity> repository = RepositoryMock();
-
+    Timestamp timestamp = Timestamp.now();
     //final entity = LyricEntityMock();
     Map<dynamic, dynamic> entity = {
       'id': 'dfsdfsd',
       'title': 'agua viva',
       'group': 'oficina g3',
+      'album-cover':
+          'http://coverartarchive.org/release/ce82afc5-f866-400e-adcd-4a23c01dd877/4433492734-500.jpg',
+      'createAt': timestamp,
       'verses': [
         {
           'id': 0,
@@ -35,7 +38,7 @@ void main() {
 
     final usecase = LyricsUseCases(repository: repository);
 
-    final result = usecase.get('');
+    final result = usecase.get('lyrics');
 
     expect(result, emits(isA<List<LyricEntity>>()));
   });
