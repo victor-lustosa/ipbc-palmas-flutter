4b1e85aaaa041c1fd462c15b7772bc204c21b1ca
Roger Zeni Antunes <89553139+rogerzen@users.noreply.github.com>
2023-11-25 19:12:35 -0300

    Refatoração page new passaword
    Refatoramos a pagina,

--- packages/auth_module/lib/src/ui/views/success_created_new_password_view.dart
+++ packages/auth_module/lib/src/ui/views/success_created_new_password_view.dart
@@ -8,48 +8,42 @@ class CreatedNewPasswordsuccess extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 162, left: 15, right: 15),
-              child: Column(
-                children: [
-                  const Image(image: AssetImage(AppImages.lock)),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  Text(
-                    'Nova senha',
-                    style: AppFonts.defaultFont(
-                      color: AppColors.grey10,
-                      fontSize: 22,
-                    ),
-                  ),
-                  Text(
-                    'Criada com sucesso!',
-                    style: AppFonts.defaultFont(
-                      color: AppColors.grey10,
-                      fontSize: 22,
-                    ),
-                  ),
-                ],
+        child: SizedBox(
+          width: context.mediaQuery.size.width,
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 162, bottom: 25),
+                  child: const Image(image: AssetImage(AppImages.lock)),
               ),
-            ),
-            const SizedBox(
-              height: 376,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-              child: ElevatedButtonWidget(
-                action: () {
-                  Modular.to.navigate('/auth');
-                },
-                backgroundColor: AppColors.darkGreen,
-                shadowColor: AppColors.grey0,
-                foregroundColor: AppColors.white,
-                child: const Padding(
-                  padding: EdgeInsets.all(12.0),
-                  child: Row(
+              Text(
+                'Nova senha',
+                style: AppFonts.defaultFont(
+                  color: AppColors.grey10,
+                  fontSize: 22,
+                ),
+              ),
+              Text(
+                'Criada com sucesso!',
+                style: AppFonts.defaultFont(
+                  color: AppColors.grey10,
+                  fontSize: 22,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 376, left: 16.0, right: 16.0),
+                child: ElevatedButtonWidget(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  fixedSize: const Size(343, 48),
+                  action: () {
+                    Modular.to.navigate('/auth');
+                  },
+                  backgroundColor: AppColors.darkGreen,
+                  shadowColor: AppColors.grey0,
+                  foregroundColor: AppColors.white,
+                  child: const Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Icon(Icons.arrow_back_ios_new),
@@ -61,8 +55,8 @@ class CreatedNewPasswordsuccess extends StatelessWidget {
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
