35de080b88c2456b44436a1fe5904a5dccb86895
Roger Zeni Antunes <89553139+rogerzen@users.noreply.github.com>
2023-12-22 17:17:55 -0300

    implementando logica cor botão e recuperando valores digitado

--- packages/auth_module/lib/src/ui/components/textfield_code_numbers.dart
+++ packages/auth_module/lib/src/ui/components/textfield_code_numbers.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:auth_module/src/ui/components/custom_border_widget.dart';
 import 'package:core_module/core_module.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +13,8 @@ class TextfieldCodeNumbers extends StatefulWidget {
 
 class _TextfieldCodeNumbersState extends State<TextfieldCodeNumbers> {
   final List<FocusNode> _focusNodes = List.generate(6, (index) => FocusNode());
-  final List<TextEditingController> _controllers =
+
+  final List<TextEditingController> controllers =
       List.generate(6, (index) => TextEditingController());
 
   @override
@@ -24,56 +27,91 @@ class _TextfieldCodeNumbersState extends State<TextfieldCodeNumbers> {
 
   @override
   Widget build(BuildContext context) {
+    List<String> textFieldValues =
+        controllers.map((controller) => controller.text).toList();
+
     return SizedBox(
-      width: 44 * 6 + 10 * 5,
-      height: 58,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: List.generate(
-          6,
-          (index) => Row(
-            children: [
-              Stack(
+      width: context.mediaQuery.size.width,
+      height: 150,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              6,
+              (index) => Row(
                 children: [
-                  CustomBorder(filled: _controllers[index].text.isEmpty),
-                  SizedBox(
-                    width: 40,
-                    height: 50,
-                    child: TextField(
-                      maxLength: 1,
-                      textAlign: TextAlign.center,
-                      keyboardType: TextInputType.number,
-                      focusNode: _focusNodes[index],
-                      controller: _controllers[index],
-                      onChanged: (value) {
-                        if (value.isEmpty) {
-                          if (index > 0) {
-                            _focusNodes[index - 1].requestFocus();
-                          }
-                        } else if (index < _focusNodes.length - 1) {
-                          _focusNodes[index + 1].requestFocus();
-                        }
-                        setState(() {});
-                      },
-                      decoration: const InputDecoration(
-                        counterText: '',
-                        contentPadding: EdgeInsets.all(10),
-                        enabledBorder: InputBorder.none, // Removendo a borda
-                        focusedBorder: InputBorder
-                            .none, // Removendo a borda quando está focado
-                        labelStyle: TextStyle(color: AppColors.darkGreen),
+                  Stack(
+                    children: [
+                      CustomBorder(filled: controllers[index].text.isEmpty),
+                      SizedBox(
+                        width: 48,
+                        height: 50,
+                        child: TextField(
+                          maxLength: 1,
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.number,
+                          focusNode: _focusNodes[index],
+                          controller: controllers[index],
+                          onChanged: (value) {
+                            if (value.isEmpty) {
+                              if (index > 0) {
+                                _focusNodes[index - 1].requestFocus();
+                              }
+                            } else if (index < _focusNodes.length - 1) {
+                              _focusNodes[index + 1].requestFocus();
+                            }
+                            setState(() {});
+                          },
+                          decoration: const InputDecoration(
+                            counterText: '',
+                            contentPadding: EdgeInsets.all(10),
+                            enabledBorder:
+                                InputBorder.none, // Removendo a borda
+                            focusedBorder: InputBorder
+                                .none, // Removendo a borda quando está focado
+                            labelStyle: TextStyle(color: AppColors.darkGreen),
+                          ),
+                          cursorColor: AppColors.darkGreen,
+                          cursorWidth: 1.5,
+                          style: const TextStyle(color: AppColors.darkGreen),
+                        ),
                       ),
-                      cursorColor: AppColors.darkGreen,
-                      cursorWidth: 1.5,
-                      style: const TextStyle(color: AppColors.darkGreen),
-                    ),
+                    ],
                   ),
+                  if (index < 5) const SizedBox(width: 10),
                 ],
               ),
-              if (index < 5) const SizedBox(width: 10),
-            ],
+            ),
           ),
-        ),
+          Container(
+            margin: const EdgeInsets.only(
+              top: 16,
+            ),
+            child: ElevatedButtonWidget(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              fixedSize: const Size(343, 48),
+              action: () {
+                if (textFieldValues.every((value) => value.isNotEmpty)) {
+                  log('TODOS OS Valores dos TextFields: $textFieldValues');
+                } else {
+                  log('Preencha todos os TextFields!');
+                }
+              },
+              backgroundColor:
+                  textFieldValues.every((value) => value.isNotEmpty)
+                      ? AppColors.darkGreen
+                      : AppColors.disableButton,
+              shadowColor: AppColors.grey0,
+              foregroundColor: AppColors.white,
+              child: const Text(
+                "Verificar",
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- packages/auth_module/lib/src/ui/views/verification_code_view.dart
+++ packages/auth_module/lib/src/ui/views/verification_code_view.dart
@@ -1,107 +1,93 @@
+import 'dart:developer';
+
 import 'package:auth_module/src/ui/components/custom_text_verification_code.dart';
 import 'package:auth_module/src/ui/components/textfield_code_numbers.dart';
 import 'package:core_module/core_module.dart';
 import 'package:flutter/material.dart';
 
-class VerificationCodePage extends StatelessWidget {
+class VerificationCodePage extends StatefulWidget {
   const VerificationCodePage({super.key});
 
+  @override
+  State<VerificationCodePage> createState() => _VerificationCodePageState();
+}
+
+class _VerificationCodePageState extends State<VerificationCodePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Center(
-        child: Column(
-          children: [
-            Stack(children: [
+      child: SingleChildScrollView(
+        child: SizedBox(
+          width: context.mediaQuery.size.width,
+          child: Column(
+            children: [
               Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.35),
-                    color: AppColors.badgeGreen),
-                margin: const EdgeInsets.only(top: 100, bottom: 24),
                 width: 100, // Define a largura como o máximo disponível
                 height: 91, // Define a altura conforme necessário
-              ),
-              Positioned.fill(
-                child: Container(
-                  margin: const EdgeInsets.only(top: 110, bottom: 24),
-                  child: const Image(
-                    image: AssetImage(
-                      AppImages.emailVerification,
-                    ),
-                    width: 60,
-                    height: 52,
+                margin: const EdgeInsets.only(top: 110, bottom: 32),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.35),
+                  color: AppColors.badgeGreen,
+                ),
+                child: const Image(
+                  image: AssetImage(
+                    AppImages.emailVerification,
                   ),
+                  width: 60,
+                  height: 52,
                 ),
               ),
-            ]),
-            Container(
-              margin: const EdgeInsets.only(
-                top: 8,
+              Container(
+                margin: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  "Verifique o seu e-mail",
+                  style: AppFonts.defaultFont(
+                      color: AppColors.grey10,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400),
+                ),
               ),
-              child: Text(
-                "Verifique o seu e-mail",
-                style: AppFonts.defaultFont(
-                    color: AppColors.grey8,
-                    fontSize: 15,
-                    fontWeight: FontWeight.w500),
-              ),
-            ),
-            CustomTextVerificationCode(
-              textOne:
-                  'Enviamos um código de recuperação de \n senha para o seu email. ',
-              styleOne: AppFonts.defaultFont(
-                color: AppColors.grey8,
-                fontSize: 15,
-              ),
-              textTwo: 'Insira-o para \n continuar.',
-              styleTwo: AppFonts.defaultFont(
-                  color: AppColors.grey10,
+              CustomTextVerificationCode(
+                textOne:
+                    'Enviamos um código de recuperação de \n senha para o seu email. ',
+                styleOne: AppFonts.defaultFont(
+                  color: AppColors.grey8,
                   fontSize: 15,
-                  fontWeight: FontWeight.w700),
-            ),
-            Container(
+                  fontWeight: FontWeight.w400,
+                ),
+                textTwo: 'Insira-o para \n continuar.',
+                styleTwo: AppFonts.defaultFont(
+                    color: AppColors.grey10,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700),
+              ),
+              Container(
                 margin: const EdgeInsets.only(top: 26.99, bottom: 16),
                 child: Text(
                   "Código de Verificação",
                   style: AppFonts.defaultFont(
                       fontSize: 18, fontWeight: FontWeight.w400),
-                )),
-            const TextfieldCodeNumbers(),
-            Container(
-              margin: const EdgeInsets.only(top: 16, bottom: 36),
-              child: ElevatedButtonWidget(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                fixedSize: const Size(343, 48),
-                action: () {
-                  Modular.to.navigate('/ ');
-                },
-                backgroundColor: AppColors.darkGreen,
-                shadowColor: AppColors.grey0,
-                foregroundColor: AppColors.white,
-                child: const Text(
-                  "Verificar",
                 ),
               ),
-            ),
-            CustomTextVerificationCode(
-                textOne:
-                    'Não recebeu o e-mail? Verifique na caixa de spam, \n ',
-                styleOne: AppFonts.defaultFont(
-                    color: AppColors.grey8,
-                    fontSize: 13,
-                    fontWeight: FontWeight.w400),
-                textTwo: 'tente novamente, clicando aqui.',
-                styleTwo: AppFonts.defaultFont(
-                    color: AppColors.darkGreen,
-                    fontSize: 13,
-                    fontWeight: FontWeight.w700),
-                onTapTextTwo: () {
-                  print('Text');
-                }),
-          ],
+              const TextfieldCodeNumbers(),
+              CustomTextVerificationCode(
+                  textOne:
+                      'Não recebeu o e-mail? Verifique na caixa de spam, \n ',
+                  styleOne: AppFonts.defaultFont(
+                      color: AppColors.fullBlack,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400),
+                  textTwo: 'tente novamente, clicando aqui.',
+                  styleTwo: AppFonts.defaultFont(
+                      color: AppColors.darkGreen,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w700),
+                  onTapTextTwo: () {
+                    log('Tentando Novamente');
+                  }),
+            ],
+          ),
         ),
       ),
     ));
