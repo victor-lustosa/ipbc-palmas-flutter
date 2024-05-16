7e8cb71f06ec70ebe5c99c379dbc6a94d7dd887f
Victor Lustosa <victor.olustosa@outlook.com>
2024-05-16 12:56:47 -0300

    refactor: refatoracao das transicoes do Modular

--- packages/core_module/lib/src/configs/app_routes.dart
+++ packages/core_module/lib/src/configs/app_routes.dart
@@ -120,14 +120,43 @@ class ModularFadeTransition extends CustomTransition {
             opaque: opaque ?? true);
 }
 
+class ModularSlideTransition extends CustomTransition {
+  ModularSlideTransition({
+    Duration? transitionDuration,
+    Duration? reverseTransitionDuration,
+    Curve? curve,
+    Offset? begin,
+    Offset? end,
+    bool? opaque,
+  }) : super(
+            transitionBuilder: (context, anim1, anim2, child) {
+              return SlideTransition(
+                position: anim1.drive(
+                  Tween(
+                    begin: begin ?? const Offset(1, 0),
+                    end: end ?? Offset.zero,
+                  ).chain(
+                    CurveTween(
+                      curve: curve ?? Curves.ease,
+                    ),
+                  ),
+                ),
+                child: child,
+              );
+            },
+            transitionDuration:
+                transitionDuration ?? const Duration(milliseconds: 300),
+            reverseTransitionDuration:
+                reverseTransitionDuration ?? const Duration(milliseconds: 300),
+            opaque: opaque ?? true);
+}
+
 class CustomFadeTransition extends PageRouteBuilder {
   final Widget child;
-  final Curve? curve;
 
   CustomFadeTransition({
     Duration? transitionSpeed,
     Duration? reverseSpeed,
-    this.curve,
     required this.child,
   }) : super(
           reverseTransitionDuration:
--- packages/events_module/lib/src/app_module.dart
+++ packages/events_module/lib/src/app_module.dart
@@ -1,5 +1,4 @@
 import 'package:core_module/core_module.dart';
-import 'package:flutter/cupertino.dart';
 
 import 'ui/views/create_event_view.dart';
 
@@ -14,16 +13,7 @@ class EventModule extends Module {
       createEventRoute,
       transition: TransitionType.custom,
       child: (_) => const CreateEventView(),
-      customTransition: CustomTransition(
-        transitionDuration: const Duration(milliseconds: 100),
-        reverseTransitionDuration: const Duration(milliseconds: 100),
-        transitionBuilder: (context, anim1, anim2, child) {
-          return FadeTransition(
-            opacity: anim1,
-            child: child,
-          );
-        },
-      ),
+      customTransition: ModularFadeTransition()
     );
   }
 }
