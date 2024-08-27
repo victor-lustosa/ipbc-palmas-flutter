98263b509929127c348a0e049732f3e7b204326c
Roger Zeni Antunes <89553139+rogerzen@users.noreply.github.com>
2024-08-27 19:57:09 -0300

    Fix: Arrumando erro de overflow quando essa pagina iniciava

--- packages/auth_module/lib/src/ui/views/reset_password_success_view.dart
+++ packages/auth_module/lib/src/ui/views/reset_password_success_view.dart
@@ -10,60 +10,62 @@ class ResetPasswordSuccessView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SizedBox(
-          width: context.mediaQuery.size.width,
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 162, bottom: 25),
-                child: const Image(
-                  image: AssetImage(AppImages.lock),
-                ),
-              ),
-              Text(
-                textAlign: TextAlign.center,
-                'Nova senha',
-                style: AppFonts.defaultFont(
-                  color: AppColors.grey10,
-                  fontSize: 22,
-                ),
-              ),
-              Text(
-                textAlign: TextAlign.center,
-                'criada com sucesso!',
-                style: AppFonts.defaultFont(
-                  color: AppColors.grey10,
-                  fontSize: 22,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: context.mediaQuery.size.height * .37,
-                  left: 16.0,
-                  right: 16.0,
-                ),
-                child: ButtonWidget(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  fixedSize: Size(MediaQuery.of(context).size.width, 48),
-                  action: () {
-                    navigate(AuthModule.authRoute + AuthModule.loginRoute);
-                  },
-                  backgroundColor: AppColors.darkGreen,
-                  shadowColor: AppColors.grey0,
-                  foregroundColor: AppColors.white,
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Continuar para o login",
-                      ),
-                    ],
+        child: SingleChildScrollView(
+          child: SizedBox(
+            width: context.mediaQuery.size.width,
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 162, bottom: 25),
+                  child: const Image(
+                    image: AssetImage(AppImages.lock),
                   ),
                 ),
-              ),
-            ],
+                Text(
+                  textAlign: TextAlign.center,
+                  'Nova senha',
+                  style: AppFonts.defaultFont(
+                    color: AppColors.grey10,
+                    fontSize: 22,
+                  ),
+                ),
+                Text(
+                  textAlign: TextAlign.center,
+                  'criada com sucesso!',
+                  style: AppFonts.defaultFont(
+                    color: AppColors.grey10,
+                    fontSize: 22,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: context.mediaQuery.size.height * .37,
+                    left: 16.0,
+                    right: 16.0,
+                  ),
+                  child: ButtonWidget(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    fixedSize: Size(MediaQuery.of(context).size.width, 48),
+                    action: () {
+                      navigate(AuthModule.authRoute + AuthModule.loginRoute);
+                    },
+                    backgroundColor: AppColors.darkGreen,
+                    shadowColor: AppColors.grey0,
+                    foregroundColor: AppColors.white,
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Continuar para o login",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
