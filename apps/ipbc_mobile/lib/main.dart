ae65e6722f0bdb3e93278a041958c86503c1ea0e
Victor Lustosa <victor.olustosa@outlook.com>
2025-05-07 00:13:42 -0300

    refactor: atualizando dependencias

--- apps/ipbc_mobile/lib/main.dart
+++ apps/ipbc_mobile/lib/main.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:home_module/home_module.dart';
 
 void main() async {
-  //Modular.setInitialRoute(AppRoutes.servicesRoute + AppRoutes.editLyricRoute);
   Modular.setInitialRoute(AppRoutes.initialRoute);
-  //Modular.setInitialRoute(AppRoutes.lyricsListRoute);
+ //Modular.setInitialRoute(AppRoutes.servicesRoute + AppRoutes.editLyricRoute);
+ //Modular.setInitialRoute(AppRoutes.lyricsListRoute);
+ //Modular.setInitialRoute(AppRoutes.authRoute + AppRoutes.loginRoute);
+
   WidgetsFlutterBinding.ensureInitialized();
-  // Modular.setInitialRoute(AppRoutes.lyricsListRoute);
   await Future.wait([SupabaseRepository.init(), IsarRepository.init()]);
   // Bloc.observer = GenericBlocObserver();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
@@ -30,35 +31,3 @@ void main() async {
     ),
   );
 }
-
-// void main() async {
-//   //  Modular.setInitialRoute(ServiceModule.servicesRoute + ServiceModule.editLyricRoute);
-//   //Modular.setInitialRoute(LyricModule.lyricsListRoute);
-//   Modular.setInitialRoute(AuthModule.authRoute + AuthModule.loginRoute);
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Future.wait([
-//     SupabaseDatasource.init(),
-//     HiveDatasource.init(),
-//   ]);
-//   Bloc.observer = GenericBlocObserver();
-//   SystemChrome.setPreferredOrientations([
-//     DeviceOrientation.portraitUp,
-//   ]).then(
-//     (_) => runApp(
-//       ModularApp(
-//         module: LyricModule(),
-//         child: MaterialApp.router(
-//           builder: (context, Widget? child) {
-//             return ScrollConfiguration(
-//               behavior: NoGlowBehavior(),
-//               child: child!,
-//             );
-//           },
-//           theme: lightTheme,
-//           routerConfig: Modular.routerConfig,
-//           debugShowCheckedModeBanner: false,
-//         ),
-//       ),
-//     ),
-//   );
-// }
