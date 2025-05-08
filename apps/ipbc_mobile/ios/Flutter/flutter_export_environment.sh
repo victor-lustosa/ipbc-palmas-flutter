cf512a3f1c9626c2466d4b212a0daab9e425e806
Victor Lustosa <victor.olustosa@outlook.com>
2025-05-07 22:29:59 -0300

    refactor: atualizando melos no projeto

--- apps/ipbc_mobile/ios/Flutter/flutter_export_environment.sh
+++ apps/ipbc_mobile/ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/victor-lustosa/Documents/Programas/flutter"
+export "FLUTTER_ROOT=/Users/victor-lustosa/fvm/versions/3.29.3"
 export "FLUTTER_APPLICATION_PATH=/Users/victor-lustosa/Documents/Projetos/Frontend/flutter_workspace/ipbc_palmas/apps/ipbc_mobile"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
--- apps/ipbc_web/android/app/src/main/kotlin/com/example/ipbc_web/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ipbc.ipbc_web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
