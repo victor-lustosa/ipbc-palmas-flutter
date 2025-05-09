8727e682aafffd3fb1b03d36aaf43b59e23d33fc
Victor Lustosa <victor.olustosa@outlook.com>
2025-05-08 22:20:41 -0300

    chore: configurando melos

--- apps/ipbc_mobile/sonar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-flutter pub get
-
-flutter test
-
-flutter test --machine --coverage > tests.output
-
-C:\Users\flustosa\Downloads\Programas\sonarqube-docker\sonar-scanner\bin\sonar-scanner.bat
-@pause
--- flutter_clean_all.sh
+++ flutter_clean_all.sh
@@ -1,54 +1,7 @@
+#!/bin/sh
+set -e
 
-echo "limpando ipbc mobile..."
-cd apps/ipbc_mobile
-#flutter packages upgrade --major-versions
+echo "Limpando módulo $PWD..."
 flutter clean
 flutter pub get
-
-echo "limpando ipbc web..."
-cd ../../apps/ipbc_web
 #flutter packages upgrade --major-versions
-flutter clean
-flutter pub get
-
-echo "limpando core module..."
-cd ../../packages/core_module
-#flutter packages upgrade --major-versions
-flutter clean
-flutter pub get
-
-echo "limpando service module..."
-cd ../../packages/service_module
-#flutter packages upgrade --major-versions
-flutter clean
-flutter pub get
-
-echo "limpando home module..."
-cd ../../packages/home_module
-#flutter packages upgrade --major-versions
-flutter clean
-flutter pub get
-
-echo "limpando auth module..."
-cd ../../packages/auth_module
-#flutter packages upgrade --major-versions
-flutter clean
-flutter pub get
-
-echo "limpando events module..."
-cd ../../packages/events_module
-#flutter packages upgrade --major-versions
-flutter clean
-flutter pub get
-
-echo "limpando lyric module..."
-cd ../../packages/lyric_module
-#flutter packages upgrade --major-versions
-flutter clean
-flutter pub get
-
-echo "limpando offers module..."
-cd ../../packages/offers_module
-#flutter packages upgrade --major-versions
-flutter clean
-flutter pub get
