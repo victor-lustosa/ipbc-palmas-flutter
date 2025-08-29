f3e2cf8ce5999b4ca1247c6fbaa0eb6c7c104237
Victor Lustosa <victor.olustosa@outlook.com>
2025-08-29 02:27:34 -0300

    refactor: adding shimmer in services list

--- packages/service_module/lib/src/ui/views/services_list_view.dart
+++ packages/service_module/lib/src/ui/views/services_list_view.dart
@@ -4,16 +4,40 @@ import 'package:flutter/cupertino.dart';
 
 class ServicesListView extends StatefulWidget {
   const ServicesListView({super.key, required this.entities});
-
   final List<ServicesEntity> entities;
 
   @override
   State<ServicesListView> createState() => _ServicesListViewState();
 }
 
-class _ServicesListViewState extends State<ServicesListView> with DateMixin {
+class _ServicesListViewState extends State<ServicesListView> with DateMixin, TickerProviderStateMixin {
+
+  late final AnimationController _shimmerController;
+
+  @override
+  void initState() {
+    super.initState();
+    _shimmerController = AnimationController.unbounded(vsync: this)
+      ..repeat(min: -0.5, max: 1.5, period: const Duration(milliseconds: 1200));
+  }
+
+  @override
+  dispose() {
+    _shimmerController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget placeholder() =>
+        ShimmerWidget(animation: _shimmerController, child: Container(
+          height: 110,
+          width: context.sizeOf.width,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ));
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -69,42 +93,55 @@ class _ServicesListViewState extends State<ServicesListView> with DateMixin {
                           context,
                         );
                       },
-                      child: Container(
-                        height: 110,
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(18),
+                      child: CachedNetworkImage(
+                        imageUrl: widget.entities[index].image,
+                        fit: BoxFit.cover,
+                        placeholder: (context, url) =>placeholder(),
+                        errorWidget: (context, url, error) =>placeholder(),
+                        color: const Color.fromRGBO(0, 66, 46, 0.40),
+                        colorBlendMode: BlendMode.color,
+                        imageBuilder: (context, imageProvider) => Container(
+                          height: 110,
+                          width: context.sizeOf.width,
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(18),
+                            ),
+                            image: DecorationImage(
+                              image: imageProvider,
+                              fit: BoxFit.cover,
+                              colorFilter: const ColorFilter.mode(
+                                Color.fromRGBO(0, 66, 46, 0.40),
+                                BlendMode.color,
+                              ),
+                            ),
                           ),
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: NetworkImage(widget.entities[index].image),
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              margin: const EdgeInsets.only(left: 16),
-                              child: Text(
-                                '${widget.entities[index].title} | ${formatHourToString(date: widget.entities[index].serviceDate)}',
-                                style: AppFonts.headline(
-                                  color: AppColors.white,
-                                  fontWeight: FontWeight.w500,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(left: 16),
+                                child: Text(
+                                  '${widget.entities[index].title} | ${formatHourToString(date: widget.entities[index].serviceDate)}',
+                                  style: AppFonts.headline(
+                                    color: AppColors.white,
+                                    fontWeight: FontWeight.w500,
+                                  ),
                                 ),
                               ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.only(right: 5),
-                              child: const IconButtonWidget(
-                                size: 34,
-                                color: AppColors.white,
-                                splashColor: Colors.transparent,
-                                highlightColor: Colors.transparent,
-                                iOSIcon: CupertinoIcons.chevron_forward,
-                                androidIcon: Icons.navigate_next_sharp,
+                              Container(
+                                margin: const EdgeInsets.only(right: 5),
+                                child: const IconButtonWidget(
+                                  size: 34,
+                                  color: AppColors.white,
+                                  splashColor: Colors.transparent,
+                                  highlightColor: Colors.transparent,
+                                  iOSIcon: CupertinoIcons.chevron_forward,
+                                  androidIcon: Icons.navigate_next_sharp,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     );
