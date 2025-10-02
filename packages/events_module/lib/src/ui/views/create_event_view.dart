9fab34859237f034cfbc271d4384e7ebe64fed5a
Juliano Vieira <julianocorreia13@gmail.com>
2025-10-01 22:46:12 -0300

    adicionando botao de loading na tela de gerenciar eventos

--- packages/events_module/lib/src/ui/views/create_event_view.dart
+++ packages/events_module/lib/src/ui/views/create_event_view.dart
@@ -21,6 +21,12 @@ class _CreateEventViewState extends State<CreateEventView> with DateMixin {
       _store.init();
 
   }
+  @override
+  dispose() {
+    _store.resetValidationFields();
+    super.dispose();
+  }
+
 
   get prefixLocationIcon => Container(
     margin: const EdgeInsets.only(left: 18, right: 10),
@@ -557,21 +563,17 @@ class _CreateEventViewState extends State<CreateEventView> with DateMixin {
                           validator: (_) => null,
                         ),
                       ),
-                      ButtonWidget(
+                      LoadingButtonWidget(
+                        margin: EdgeInsets.zero,
+                          isPressed: _store.isAddEventPressed,
                         action: () async {
                           final response = await _store.addData(context);
                           if (response) {
                             updateCallback();
                           }
                         },
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        fixedSize: Size(context.sizeOf.width, 48),
-                        backgroundColor: AppColors.darkGreen,
-                        shadowColor: AppColors.grey0,
-                        foregroundColor: AppColors.white,
-                        child: const Text("Salvar evento"),
+                        label: "Salvar evento",
+                        isValid: _store.isAllFieldsValid,
                       ),
                       Visibility(
                         visible: _store.isEditing,
