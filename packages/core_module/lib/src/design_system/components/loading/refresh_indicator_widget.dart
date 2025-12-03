5c320c5a04cef64ba272660a0b770cf546d9eca8
Victor Lustosa <victor.olustosa@outlook.com>
2025-12-03 00:41:12 -0300

    refactor: refactoring reload scroll components[2]

--- packages/core_module/lib/src/design_system/components/loading/refresh_indicator_widget.dart
+++ packages/core_module/lib/src/design_system/components/loading/refresh_indicator_widget.dart
@@ -22,13 +22,119 @@ class RefreshIndicatorWidget extends StatelessWidget {
   @override
   Widget build(context) {
     return Platform.isAndroid
-        ? RefreshIndicator(
-            elevation: elevation ?? .7,
-            backgroundColor: backgroundColor ?? AppColors.white,
-            color: color ?? AppColors.darkGreen,
-            onRefresh: action,
-            child: child,
-          )
+        ? CustomNativeRefresh(onRefresh: action, child: child)
         : child;
+  }
+}
+
+class CustomNativeRefresh extends StatefulWidget {
+  final Widget child;
+  final Future<void> Function() onRefresh;
+
+  const CustomNativeRefresh({
+    super.key,
+    required this.child,
+    required this.onRefresh,
+  });
+
+  @override
+  State<CustomNativeRefresh> createState() => _CustomNativeRefreshState();
+}
+
+class _CustomNativeRefreshState extends State<CustomNativeRefresh> {
+  bool _isRefreshing = false;
+  double _dragOffset = 0.0;
+
+  final double _triggerDistance = 80.0;
+  final double _maxDragDistance = 120.0;
+
+  bool _handleScrollNotification(ScrollNotification notification) {
+    if (notification is ScrollUpdateNotification) {
+      if (notification.metrics.pixels < 0) {
+        final double overscroll = notification.metrics.pixels.abs();
+        if (!_isRefreshing) {
+          setState(() {
+            _dragOffset = overscroll.clamp(0.0, _maxDragDistance);
+          });
+        }
+      } else if (_dragOffset > 0 && !_isRefreshing) {
+        setState(() {
+          _dragOffset = 0.0;
+        });
+      }
+    } else if (notification is ScrollEndNotification) {
+      if (_dragOffset >= _triggerDistance && !_isRefreshing) {
+        _startRefresh();
+      } else {
+        _reset();
+      }
+    }
+    return false;
+  }
+
+  void _startRefresh() async {
+    setState(() {
+      _isRefreshing = true;
+      _dragOffset = _triggerDistance;
+    });
+
+    try {
+      await widget.onRefresh();
+    } finally {
+      if (mounted) {
+        _reset();
+      }
+    }
+  }
+
+  void _reset() {
+    setState(() {
+      _isRefreshing = false;
+      _dragOffset = 0.0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        NotificationListener<ScrollNotification>(
+          onNotification: _handleScrollNotification,
+          child: widget.child,
+        ),
+
+        if (_dragOffset > 0)
+          Positioned(
+            top: _dragOffset - 45,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Container(
+                width: 45,
+                height: 45,
+                decoration: BoxDecoration(
+                  color: AppColors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withAlpha(10),
+                      blurRadius: 2,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: const LoadingWidget(
+                    size: 22,
+                    androidRadius: 2.5,
+                    iosRadius: 10,
+                    color: AppColors.darkGreen,
+                  ),
+                ),
+              ),
+            ),
+          ),
+      ],
+    );
   }
 }
